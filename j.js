d217902319a7568cb04ed80c0a666048e2d2ae7c
Henry Mao <122509674+henhenmao@users.noreply.github.com>
2024-10-07 02:58:03 -0700

    Created rock-paper-scissors game - first time using javascript - practiced how to use functions - basically practiced the basic syntax - can play rock paper scissors in the inspect element console
    program
    - get user input for rock paper or scissors
    - computer randomly chooses rock paper or scissors
    - human and computer choices are compared and someone is declared a winner (or tie)
    - above 3 steps are repeated 5 times for a full rock paper scissors match
    - score is counted after each round

---
+++ j.js
@@ -0,0 +1,54 @@
+function getComputerChoice() {
+    let rand = Math.floor(Math.random() * 3);
+    if (rand == 0) {
+        return "rock";
+    } else if (rand == 1) {
+        return "paper";
+    } else {
+        return "scissors";
+    }
+
+}
+
+function getHumanChoice() {
+    let choice = prompt("rock, paper, or scissors? ");
+    return choice;
+}
+
+
+function playRound(humanChoice, computerChoice) {
+    console.log(`Computer went ${computerChoice}!!!`)
+    if ((humanChoice == "rock" && computerChoice == "scissors") ||
+        (humanChoice == "paper" && computerChoice == "rock") ||
+        (humanChoice == "scissors" && computerChoice == "paper")) {
+        console.log(`You win! ${humanChoice} beats ${computerChoice}!!`);
+        return 1;
+    } else if ((humanChoice == "rock" && computerChoice == "paper") ||
+        (humanChoice == "paper" && computerChoice == "scissors") ||
+        (humanChoice == "scissors" && computerChoice == "rock")) {
+        console.log(`You lost! ${computerChoice} beats ${humanChoice}!!`);
+        return -1
+    } else {
+        console.log(`You tied! ${humanChoice} ties ${computerChoice}`);
+        return 0;
+    }
+}
+
+
+function playGame() {
+    let humanScore = 0;
+    let computerScore = 0;
+    for (let i = 1; i <= 5; i += 1) {
+        console.log(`Round ${i}!!!`);
+        game = playRound(getHumanChoice().toLowerCase(), getComputerChoice());
+        if (game == 1) {
+            humanScore += 1;
+        } else if (game == -1) {
+            computerScore += 1;
+        }
+        console.log(`You: ${humanScore} - Computer: ${computerScore}`);
+        console.log();
+    }
+}
+
+playGame();
